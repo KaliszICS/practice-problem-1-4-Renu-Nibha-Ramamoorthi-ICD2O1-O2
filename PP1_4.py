639b9de59318b825e62244e266ba94ccab7639bf
Renu-Nibha-Ramamoorthi-ICD2O1-O2 <renunibha.ramamoorthi@student.tdsb.on.ca>
2024-11-05 14:29:09 -0500

    Update PP1_4.py

--- PP1_4.py
+++ PP1_4.py
@@ -2,21 +2,21 @@
 
 def q1():
   #Write Assignment code here
-  word = input("Input a word: ") 
+  word = input("Input a word:") 
   print(word) 
 def q2():
   #Write Assignment code here
-  First_name = input("Input your first name: ") 
+  First_name = input("Input your first name:") 
   print("Hello " + first_name) 
 def q3():
   #Write Assignment code here
-  first_name = input("Input your first name: ") 
-  last_name = input("Input your last name: ") 
+  first_name = input("Input your first name:") 
+  last_name = input("Input your last name:") 
   print(Last_name, First_name) 
 def q4():
   #Write Assignment code here
-  student1 = input("Input a student") 
-  student2 = input("Input another student: ") 
+  student1 = input("Input a student:") 
+  student2 = input("Input another student:") 
   print("Your students are", Student1, "and", student2) 
 #Comment Out the go below when you run your test cases.
 #Do not comment it out when you want to run your code normally
